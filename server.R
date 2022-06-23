b5f2c2ae9bf395bcc49a1f4c032e36594cba3888
MoritzStengel <91943443+FFTibet@users.noreply.github.com>
2022-06-23 13:46:59 +0200

    Making tables dynamic with alex

--- server.R
+++ server.R
@@ -8,15 +8,38 @@ server <- function(input, output) {
     # initialize given cancer data via cancer_data.csv
     df <- read.csv("cancer_data.csv", header =TRUE)
     
-    # giva an output of the dataframe
-    output$tableCancer <- renderTable(head(df))
+    # Filter table 
+    data <- reactive(df %>% select.list(c(input$tab_opt)))
     
+    
+    # giva an output of the dataframe
+    output$tableCancer <- renderTable({
+      
+      # creating subset of dataframe, min.value of slider input < df$age <  max.value of slider input
+      ageintervall <-
+        subset(df, df$age > input$inage[1] & df$age < input$inage[2])
+      
+      # give columns every column name if nothing is selected, result = show all columns
+      columns = names(df)
+      
+      # if tab_opt is not empty then overwrite columns with the current selections
+      if (!is.null(input$tab_opt)) {
+        columns = input$tab_opt
+      }
+      
+      # set dataframe with the selected columns
+      ageintervall[order(ageintervall$age), columns, drop = FALSE]
+      
+    })
+    
+    
+     
     
     # generate input smoking based on input$smoking from ui.R
     output$smokersPlot <- renderPlot({
       
       df %>% 
-        ggplot(aes( x=age, y=smoking)) +
+        ggplot(aes( x = age, y=smoking)) +
         geom_col() +
         theme_bw() +
         labs( x = "Patient Age in Years",
--- ui.R
+++ ui.R
@@ -1,10 +1,10 @@
 library(shiny)
 library(shinythemes)
 
-# Define UI for Aplication that shows ALL
+# Define UI for Application that shows ALL
 ui <- fluidPage(
   
-  
+  theme = shinytheme("slate"),
   
   # Application title
   titlePanel("ALL-Dashboard"),
@@ -15,42 +15,69 @@ ui <- fluidPage(
     # set tab Panel 1
     tabPanel("Tab 1",
 
-  # sidebar Panel
-  sidebarLayout(
-    sidebarPanel(h4("This is our SidebarPanel:"),
-                 p("This is the section where our Users can choose the things they want to investigate"),
-                 helpText("Toggle your Option on/off wich you want to see in the table"),
-                 checkboxGroupInput(
-                   "tab_opt", "Table toggle Options", 
-                   c("age" = "age",
-                     "Cholesterine" = "chol",
-                     "Diet" = "diet",
-                     "sex" = "gender",
-                     "height" = "height",
-                     "Smoker" = "smoking",
-                     "Tumor Size" = "tumour_size",
-                     "weight" = "weight",
-                     "ID" = "ID")
-                 )
-                 
-                 ),
+          # sidebar Panel
+          sidebarLayout(
+            sidebarPanel(h4("This is our SidebarPanel:"),
+                         p("This is the section where our Users can choose the things they want to investigate"),
+                         helpText("Toggle your Option on/off wich you want to see in the table"),
+                         checkboxGroupInput(
+                           "tab_opt", "Table toggle Options", 
+                           c("age" = "age",
+                             "Cholesterine" = "chol",
+                             "Diet" = "diet",
+                             "sex" = "gender",
+                             "height" = "height",
+                             "Smoker" = "smoking",
+                             "Tumor Size" = "tumour_size",
+                             "weight" = "weight",
+                             "ID" = "ID"),
+                           selected = 
+                             c("age" = "age",
+                               "Cholesterine" = "chol",
+                               "Diet" = "diet",
+                               "sex" = "gender",
+                               "height" = "height",
+                               "Smoker" = "smoking",
+                               "Tumor Size" = "tumour_size",
+                               "weight" = "weight",
+                               "ID" = "ID")
+                         ),
+                         selectInput("sel_opt",
+                                     "Sort your table",
+                                     choices = c("Ascedent"= "asc",
+                                                 "Descedent" = "desc"),
+                         ),
+                         sliderInput("inage",
+                                     "Age Intervall",
+                                     min = 0,
+                                     max = 100,
+                                     value = c(1,30))
+            ),
+            
     
-    # Show a plot of the smokers and years 
-    mainPanel(h1("This is our Main Panel:", align = "center"),br(),
-              h5(strong("This Plot shows an Example of Smokers and their age:")),br(),
-                  plotOutput("smokersPlot"),br(),
-              h5(strong("This example show the Head of our Database of Cancer Data:")),br(),
-                  tableOutput("tableCancer")
-              
-              ) # main panel closing bracket
+            # Show a plot of the smokers and years 
+            mainPanel(h1("This is our Main Panel:", align = "center"),br(),
+                      h5(strong("This Plot shows an Example of Smokers and their age:")),br(),
+                          plotOutput("smokersPlot"),br(),
+                      h5(strong("This example show the Head of our Database of Cancer Data:")),br(),
+                          tableOutput("tableCancer")
+                      
+                      ) # main panel closing bracket
     
-      ) # side bar layout closing bracket
+            ) # side bar layout closing bracket
     
     ), # tab Panel 1 closing bracket  
   
     # set tab panel 2
-    tabPanel("Tab 2",
-             p("This section has no content yet")
+    tabPanel("About Leukämie", br(),
+             p(tags$a(href="https://flexikon.doccheck.com/de/Leukämie", "doccheck")),
+             h3('About this R Shiny App'),br(),
+             p('Shiny Visualization. R Shiny App for ALL dataset'),br(), 
+             p('Wahlfach R und Shiny Apps, Sommersemester 2022'),br(), 
+             p('Version 1.0'),br(),
+             p('Last update: 23-MAY-2022'),br(),
+             p('Used packages: tidyverse (1.3.0), shiny (1.6.0)'),br(), 
+             p('Author: Moritz Stengel')
       
     ), # tab Panel 2 closing bracket
   
